0925b41e582d3124476402f36bbf0b2b0f3b4982
vitorrafael <vrsilveira07@gmail.com>
2025-02-09 18:53:31 -0300

    chore: remove unnecessary repositories

--- src/core/ports/CustomerRepository.d.ts
+++
@@ -1,7 +0,0 @@
-import CustomerDTO from "../../core/customers/dto/CustomerDTO";
-
-export default interface CustomerRepository {
-  create(customerDTO: CustomerDTO): Promise<CustomerDTO | undefined>;
-  findByCPF(cpf: string): Promise<CustomerDTO | undefined>;
-  findById(id: number): Promise<CustomerDTO | undefined>;
-}
